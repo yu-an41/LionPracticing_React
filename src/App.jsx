ce140b16a88700e8a940e9ac513d7244ba4d867c
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-03-09 12:13:52 +0800

    edit route for order/ calendar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // pages
 import OrderDetails from './02-rwd/OrderDetails_Group'
+import Calendar from './04-calendar/Calendar'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/' element={<OrderDetails/>}/>
+      <Route path='/order' element={<OrderDetails/>}/>
+      <Route path='/calendar' element={<Calendar/>}/>
       </Routes>
     </BrowserRouter>
   )
