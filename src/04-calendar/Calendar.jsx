73db184f01a609d512d9d782ca1ef80ddf3b5df1
Yvonne Cheng <yvonne840401tw@gmail.com>
2023-03-09 15:19:45 +0800

    add fontawesome

--- src/04-calendar/Calendar.jsx
+++ src/04-calendar/Calendar.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 function Calendar() {
   return (
-    <div>Calendar</div>
+    <div>
+      <div className="top_container">
+        <div className="prev_month">
+          <div className="arrow prev_arrow"></div>
+        </div>
+        <div className="months_tabs">
+          <div className="monthly_tab">
+            <div className="tab_container prev_tab"></div>
+            <div className="tab_container current_tab"></div>
+            <div className="tab_container next_tab"></div>
+          </div>
+        </div>
+        <div className="next_month">
+        <div className="arrow next_arrow"></div>
+        </div>
+      </div>
+      <div className="bottom_container">
+
+      </div>
+    </div>
   )
 }
 
-export default Calendar
+export default Calendar 
